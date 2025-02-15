b697f7b30d622c89059ff77bd04ee1e9cf5e405c
br14nn <vituallabrian727@gmail.com>
2025-02-15 23:12:25 +0800

    initialize

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 flex w-full items-center justify-center bg-amber-100 shadow-lg">
+      Navbar
+    </nav>
+  );
+};
+
+export default Navbar;
