b1ffe163ca2e39eb1e49ba1a1df6814b53ec8e86
br14nn <vituallabrian727@gmail.com>
2025-02-16 16:07:07 +0800

    started designing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import pfp from "@/images/pfp.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
-      <h1>Home</h1>
+      <section className="flex min-h-screen w-full max-w-[1600px] items-center justify-center p-4">
+        <Image className="select-none" src={pfp} alt="pfp" />
+      </section>
     </main>
   );
 }
